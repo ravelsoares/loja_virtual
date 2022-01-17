048105f906b9a88db4edc97a819e6b13260ee184
ravelsoares <ravelsoares64@gmail.com>
2022-01-17 10:51:40 -0300

    Grade e lista de produtos

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/datas/product_data.dart';
+import 'package:loja_virtual/tiles/product_tile.dart';
 
 class CategoryScreen extends StatelessWidget {
   final DocumentSnapshot snapshot;
@@ -27,12 +29,47 @@ class CategoryScreen extends StatelessWidget {
             ],
           ),
         ),
-        body: TabBarView(
-          physics: const NeverScrollableScrollPhysics(),
-          children: [
-            Container(),
-            Container(),
-          ],
+        body: FutureBuilder<QuerySnapshot>(
+          future: Firestore.instance
+              .collection('products')
+              .document(snapshot.documentID)
+              .collection('items')
+              .getDocuments(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Center(child: CircularProgressIndicator());
+            } else {
+              return TabBarView(
+                physics: const NeverScrollableScrollPhysics(),
+                children: [
+                  GridView.builder(
+                    padding: const EdgeInsets.all(4),
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 4,
+                      crossAxisSpacing: 4,
+                      childAspectRatio: 0.65,
+                    ),
+                    itemCount: snapshot.data.documents.length,
+                    itemBuilder: (context, index) {
+                      return ProductTile(
+                        'grid',
+                        ProductData.fromDocument(
+                            snapshot.data.documents[index]),
+                      );
+                    },
+                  ),
+                  ListView.builder(itemBuilder: (context, index) {
+                    return ProductTile(
+                      'list',
+                      ProductData.fromDocument(snapshot.data.documents[index]),
+                    );
+                  }),
+                ],
+              );
+            }
+          },
         ),
       ),
     );
---
+++ lib/tiles/product_tile.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:loja_virtual/datas/product_data.dart';
+
+class ProductTile extends StatelessWidget {
+  ProductTile(this.type, this.data);
+
+  final String type;
+  final ProductData data;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
